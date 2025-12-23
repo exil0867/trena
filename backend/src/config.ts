48851899c83e267e1d21259ebbb07bd4cb8e986e
Exil <exil@kyrena.dev>
2025-12-23 01:36:29 +0100

    Update environment variables

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,4 @@
 export const config = {
     port: 3004,
-    supabaseUrl: process.env.SUPABASE_URL || 'http://127.0.0.1:54221',
-    supabaseKey: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU',
-    jwtSecret: process.env.SUPABASE_JWT_SECRET || 'super-secret-jwt-token-with-at-least-32-characters-long',
+    jwtSecret: process.env.JWT_SECRET || 'super-secret-jwt-token-with-at-least-32-characters-long',
 };
